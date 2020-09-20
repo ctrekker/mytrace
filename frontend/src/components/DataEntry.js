df5a7a48f185e5beb2f046e3af3cf40ae413d4ea
Ianomatic1 <ianomatic1@gmail.com>
2020-09-19 18:39:33 -0600

    Data Entry BackEnd integration

--- frontend/src/components/DataEntry.js
+++ frontend/src/components/DataEntry.js
@@ -2,24 +2,25 @@ import React, {useState} from "react";
 import {Modal, Form, Button, Input, DatePicker, Cascader} from "antd";
 import {PlusOutlined} from '@ant-design/icons';
 import "./DataEntry.css"
+import Config from "../Config";
+import moment from 'moment-es6';
 
 function DataEntry(props) {
     const [visible, setVisible] = useState(false);
     const [formVariant, setFormVariant] = useState();
-    let conversionRate = 1;
 
     const options = ([
         {
-            value: 'ELECTRICITY',
-            label: "Electricty"
+            value: 'ELECTRIC',
+            label: "Electricity"
         },
         {
-                value: "NATURAL_GAS",
-                label: "Natural Gas"
+            value: "NATURAL_GAS",
+            label: "Natural Gas"
         },
         {
-                value:"TRANSPORTATION",
-                label:"Transportation"
+            value:"TRANSPORTATION",
+            label:"Transportation"
         }
 
     ]);
@@ -28,25 +29,26 @@ function DataEntry(props) {
         setFormVariant(value.toString());
     }
 
-    function onOk() {
+    async function onOk() {
         if(document.getElementById(formVariant).value.match(/^[0-9]+([.][0-9]+)?$/g) && document.getElementById("date").value.length > 0) {
-            switch(formVariant){
-                case("ELECTRICITY"):{
-                    conversionRate = 1.5588;
-                    break
-                }
-                case("NATURAL_GAS"):{
-                    conversionRate = 12.1033638;
-                    break
-                }
-                default:
-                    conversionRate = 19.59
-            }
-            setVisible(false);
+            Config.sendRequest('/api/metrics', 'POST', {
+                source: formVariant,
+                metricTimestamp: moment(document.getElementById('date').value).toDate().getTime(),
+                data: document.getElementById(formVariant).value
+            })
+                .then(res => {
+                    console.log(res);
+                    setVisible(false);
+                    setFormVariant(null);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
     function onCancel() {
         setVisible(false);
+        setFormVariant(null);
     }
 
     return (
@@ -57,8 +59,8 @@ function DataEntry(props) {
             <Modal title="Data Input" visible={visible} onOk={onOk} onCancel={onCancel} destroyOnClose={true}>
                 <Cascader options={options} onChange={onChange} placeholder="Select One" allowClear={false} style={{display:"flex", alignItems:"center", textAlign:"center", marginBottom:"2%"}} id = "type"/>
                 <Form style={{display:"-ms-inline-flexbox", flexDirection:"row", flexWrap:"nowrap"}}>
-                    { formVariant === 'ELECTRICITY' && (
-                        <Form.Item><Input id={"ELECTRICITY"} placeholder={"Kilowatt Hours"}/></Form.Item>
+                    { formVariant === 'ELECTRIC' && (
+                        <Form.Item><Input id={"ELECTRIC"} placeholder={"Kilowatt Hours"}/></Form.Item>
                     )}
                     { formVariant === 'NATURAL_GAS' && (
                         <Form.Item><Input id={"NATURAL_GAS"} placeholder={"Centum Cubic Feet"}/></Form.Item>
